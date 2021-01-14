99ed31f4885f293d669a07ffb98131de8aa44273
Chris Bailey <chris85bailey@aol.co.uk>
2021-01-14 09:21:58 +0000

    added key events

--- dist/script.js
+++ dist/script.js
@@ -2,15 +2,28 @@ const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const slideOne = document.querySelector('.slide-one');
 const slideTwo = document.querySelector('.slide-two');
-next.addEventListener('click', function () {
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.code)
+    if (e.code === 'ArrowRight') {
+        nextSlide();
+    } else if (e.code === 'ArrowLeft') {
+        prevSlide();
+    }
+});
+
+function nextSlide() {
     if (slideOne.classList[2] === 'active') {
         slideOne.classList.remove('active');
         slideTwo.classList.add('active');
     }
-})
-prev.addEventListener('click', function () {
+}
+
+function prevSlide() {
     if (slideTwo.classList[2] === 'active') {
         slideTwo.classList.remove('active');
         slideOne.classList.add('active');
     }
-})
+}
